9c730afe215bfc5ec9f00f8fd586323e5fe8dfe4
Shreyas Sahoo <shreyassahoo26@gmail.com>
2024-05-08 13:24:49 +0530

    Macro View Added

---
+++ MacroTracker/MacroItemView.swift
@@ -0,0 +1,18 @@
+//
+//  MacroItemView.swift
+//  MacroTracker
+//
+//  Created by Shreyas Sahoo on 08/05/24.
+//
+
+import SwiftUI
+
+struct MacroItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MacroItemView()
+}
---
+++ MacroTracker/OpenAIService.swift
@@ -0,0 +1,8 @@
+//
+//  OpenAIService.swift
+//  MacroTracker
+//
+//  Created by Shreyas Sahoo on 08/05/24.
+//
+
+import Foundation
